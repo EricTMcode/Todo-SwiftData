989dc1c07537fdb8bb8feaa28a722829aa78a259
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-02-24 14:17:32 +0100

    Create ContentView

---
+++ Todo-SwiftData/Screens/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Todo-SwiftData
+//
+//  Created by Eric on 24/02/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var categories: [TodoCategory]
+
+    @State private var showingAddCategory = false
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(categories) { category in
+                    NavigationLink {
+//                        TaskListView(category: category)
+                    } label: {
+                        Text(category.title)
+                    }
+                }
+                .onDelete(perform: deleteCategories)
+            }
+            .navigationTitle("Todo Categories")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showingAddCategory = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .sheet(isPresented: $showingAddCategory) {
+//                AddCategoryView()
+            }
+        }
+    }
+
+    private func deleteCategories(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { categories[$0] }.forEach(modelContext.delete)
+            try? modelContext.save()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
