1766a95e2cb8f21a585f67308d010bc6875ccece
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-02-24 14:05:28 +0100

    Initial Commit

---
+++ Todo-SwiftData/Todo_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_SwiftDataApp.swift
+//  Todo-SwiftData
+//
+//  Created by Eric on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Todo_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
